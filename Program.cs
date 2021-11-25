f867445b9f7b5d62afdebeb5de245f5d987e8a1e
Danila06 <93424789+Danila06@users.noreply.github.com>
2021-11-25 21:08:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bocharov_pr3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
